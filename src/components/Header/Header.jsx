2d30e1a49dc035211a50379370f559274a52ad10
arielnasimba <arielkafe@hotmail.com>
2024-03-07 13:46:51 +0100

    first commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,53 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+
+    <>
+     <header className={` w-full h-[7rem] 
+
+
+                            
+                                  min-[1440px]:  min-[1440px]:            
+                                    min-[1440px]:  min-[1440px]:
+     
+     `}
+     
+     >
+
+        <ul className={` 
+        
+        
+        
+                            min-[1440px]:w-full     min-[1440px]:bg-slate-400  min-[1440px]:flex  min-[1440px]:px-[7rem]
+                            min-[1440px]:h-full     min-[1440px]:items-center min-[1440px]: justify-between 
+        
+        `}>
+            
+            <li className={`
+                                        min-[1440px]:w-[19%] h-[40%] bg-stone-600
+            `}>
+                <h1 className={`            
+                                            
+                                            min-[1440px]:text-[2rem]
+                
+            `}>
+                    
+                    Where in the World?
+                    
+                </h1>
+            </li>
+            
+            <li className={`
+                                        min-[1440px]:w-[19%] h-[40%] bg-stone-600
+            `}>
+
+            </li>
+        </ul>
+
+
+     </header>
+    
+    </>
+  )
+}
---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import Header from '../Header/Header'
+
+export default function Home() {
+  return (
+
+    <>
+        <body className={`w-[100dvw] min-h-[100dvh] `}
+
+           
+        
+        
+        
+        >
+
+<Header />
+            
+        </body>
+    
+    </>
+  )
+}
