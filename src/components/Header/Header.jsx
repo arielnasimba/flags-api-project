618917751e01aae09a42220eddbc88ba8d162311
arielnasimba <arielkafe@hotmail.com>
2024-03-07 14:49:26 +0100

    add header component and template home page component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import MOON from "../../assets/images/moon.png"
 
 export default function Header() {
   return (
 
     <>
-     <header className={` w-full h-[7rem] 
+     <header className={` w-full h-[7rem]  shadow-[0px_3px_3px_0px_rgba(201,190,190,0.3)]
+     
+        box-shadow: 0px 7px 0px -4px rgba(219,202,202,1);
 
 
                             
-                                  min-[1440px]:  min-[1440px]:            
+                                  min-[1440px]:flex  min-[1440px]:justify-center            
                                     min-[1440px]:  min-[1440px]:
      
      `}
@@ -19,28 +23,62 @@ export default function Header() {
         
         
         
-                            min-[1440px]:w-full     min-[1440px]:bg-slate-400  min-[1440px]:flex  min-[1440px]:px-[7rem]
+                            min-[1440px]:w-[88%]     min-[1440px]:  min-[1440px]:flex  min-[1440px]:
                             min-[1440px]:h-full     min-[1440px]:items-center min-[1440px]: justify-between 
         
         `}>
+
+          <Link
+
+          to={`/flags-api-project/`}
+
+          className={`
+                                  min-[1440px]:w-[19%] min-[1440px]:h-[40%] 
+                `}
+          >
+          
+              <h1 className={`            
+
+                                          min-[1440px]:text-[2.1rem]  min-[1440px]:font-[900]
+
+              `}>
+
+                  Where in the world?
+
+              </h1>
+          
+        
+          </Link>
+
             
             <li className={`
-                                        min-[1440px]:w-[19%] h-[40%] bg-stone-600
+                                        min-[1440px]:w-[19%] min-[1440px]:h-[40%] 
+                                        min-[1440px]:flex min-[1440px]:justify-end
             `}>
-                <h1 className={`            
-                                            
-                                            min-[1440px]:text-[2rem]
+
+              <button className={`  
+
+                                              min-[1440px]:btn min-[1440px]:h-full min-[1440px]:w-[55%]
+                                              min-[1440px]: glass
+              `}>
+
+                <img src={MOON} alt="" srcset=""
+                    className={`
+                    
+                                          min-[1440px]:w-[12%]
+                    `}
+                />
+                <span  className={` 
+
+
+                                  min-[1440px]:text-[1.3rem]  min-[1440px]:font-[600]
                 
-            `}>
-                    
-                    Where in the World?
-                    
-                </h1>
-            </li>
-            
-            <li className={`
-                                        min-[1440px]:w-[19%] h-[40%] bg-stone-600
-            `}>
+                `}
+                
+                >
+                  Dark Mode
+                </span>
+              </button>
 
             </li>
         </ul>
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
 import Header from '../Header/Header'
+import '../Home/Home.css'
 
 export default function Home() {
   return (
 
     <>
-        <body className={`w-[100dvw] min-h-[100dvh] `}
+        <body className={`w-[100dvw] min-h-[100dvh] 
+
+                                    min-[1440px]:
+                                    min-[1440px]:flex min-[1440px]:flex-col
+        
+        
+        
+        `}
 
            
         
@@ -14,6 +22,63 @@ export default function Home() {
         >
 
 <Header />
+
+
+      <main className={`w-[88%]  
+
+                        min-[1440px]:min-h-[100dvh]
+                      min-[1440px]: min-[1440px]:self-center
+
+
+
+      `}
+      >
+
+        <div className={`search_filter_area 
+        
+                        min-[1440px]:w-full min-[1440px]:min-[1440px]:h-[12rem]
+          `}>
+
+            <ul className={` 
+
+
+
+                    min-[1440px]:w-full     min-[1440px]:  min-[1440px]:flex  min-[1440px]:
+                    min-[1440px]:h-full     min-[1440px]:items-center min-[1440px]: justify-between 
+
+            `}>
+            
+              <li
+              className={`
+                            min-[1440px]:w-[40%] min-[1440px]:h-[45%] min-[1440px]:bg-red-400
+              `}
+              >
+
+
+
+              </li>
+
+
+            <li className={`
+                                min-[1440px]:w-[19%] min-[1440px]:h-[45%] min-[1440px]:bg-red-400
+                                min-[1440px]:flex min-[1440px]:justify-end 
+            `}>
+            
+
+
+
+            </li>
+            </ul>
+
+
+
+
+
+        </div>
+        
+
+      </main>
+
             
         </body>
     
